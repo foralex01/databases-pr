f86a0b7bf320e379503dcde65bc2c755f47386d9
foralex01 <adm8mwh@virginia.edu>
2023-04-16 21:14:40 -0400

    added the connect-to-db file

---
+++ connect-db.php
@@ -0,0 +1,34 @@
+<?php
+
+
+/** S23, PHP (on GCP, local XAMPP, or CS server) connect to MySQL (on CS server) **/
+$username = 'your-computingID'; 
+$password = 'your-mysql-password';
+$host = 'mysql01.cs.virginia.edu';
+$dbname = 'your-computingID';
+$dsn = "mysql:host=$host;dbname=$dbname";
+////////////////////////////////////////////
+
+
+try 
+{
+//  $db = new PDO("mysql:host=$hostname;dbname=db-demo", $username, $password);
+   $db = new PDO($dsn, $username, $password);
+   
+   // dispaly a message to let us know that we are connected to the database 
+   echo "<p>You are connected to the database: $dsn</p>";
+}
+catch (PDOException $e)     // handle a PDO exception (errors thrown by the PDO library)
+{
+   // Call a method from any object, use the object's name followed by -> and then method's name
+   // All exception objects provide a getMessage() method that returns the error message 
+   $error_message = $e->getMessage();        
+   echo "<p>An error occurred while connecting to the database: $error_message </p>";
+}
+catch (Exception $e)       // handle any type of exception
+{
+   $error_message = $e->getMessage();
+   echo "<p>Error message: $error_message </p>";
+}
+
+?>
