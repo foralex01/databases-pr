4c12beadc76459b1f17abd6bb7d2ab7259907878
Henry Todd <htodd2001@gmail.com>
2023-04-28 10:44:25 -0400

    login redirects to home.php

--- login.php
+++ login.php
@@ -6,7 +6,7 @@ session_start();
  
 // Check if the user is already logged in, if yes then redirect him to welcome page
 if(isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true){
-    header("location: welcome.php"); // change from welcome.php to whatever we want our first page to be
+    header("location: home.php"); // change from welcome.php to whatever we want our first page to be
     exit;
 }
  
