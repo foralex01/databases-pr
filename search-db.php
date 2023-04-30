57ecf1845662d7821d40c62335afcce59f791c07
Matthew Lunsford <37967039+mattrlunsford@users.noreply.github.com>
2023-04-30 13:29:46 -0400

    filtering works (mostly)

--- search-db.php
+++ search-db.php
@@ -1,20 +1,123 @@
+<!-- Logic for multiple conditions adapted from: https://stackoverflow.com/questions/13206530/search-filtering-with-php-mysql -->
+
 <?php
 
-function searchCourses($searchText)
+function searchCourses($postVariables)
 {
     global $db;
 	// define query
-	$query = "SELECT * FROM Course NATURAL JOIN Section WHERE dept_abbr LIKE :searchText OR course_code LIKE :searchText OR course_name LIKE :searchText OR description LIKE :searchText";
+
+    // piece together filters
+    echo $searchText = $postVariables['searchText'];
+    echo $semesterYear = $postVariables['semesterYearFilter'];
+    echo $dept = $postVariables['deptFilter'];
+    $semester;
+    $year;
+
+    $query = "SELECT * FROM Course NATURAL JOIN Section";
+    $conditions = array();
+
+    if (!empty($semesterYear) && $semesterYear != "Filter by Semester...") {
+        // echo $semesterYear; // looks good
+        [$semester, $year] = parseSemesterYear($semesterYear);
+
+        $conditions[] = "semester = :semester";
+        $conditions[] = "year = :year";
+    }
+    if (!empty($searchText))
+    {
+        $conditions[] = "(dept_abbr LIKE :searchText OR course_code LIKE :searchText OR course_name LIKE :searchText OR description LIKE :searchText)";
+    }
+    if (!empty($dept)) {
+        $conditions[] = "dept_abbr = :dept";
+    }
+
+    // add conditions
+    if (count($conditions) > 0) {
+        $query .= " WHERE " . implode(' AND ', $conditions);
+        echo $query;
+    }
+
     // prepare query
 	$statement = $db->prepare($query);
     $statement->bindValue(':searchText', '%'.$searchText.'%');
-	// execute
+    $statement->bindValue(':semester', $semester);
+    $statement->bindValue(':year', $year);
+    $statement->bindValue(':dept', $dept);
+	
+    // execute
     $statement->execute();
 	$data = $statement->fetchAll(); // fetchAll() gets all the rows, fetch() gets just the first row
 	// close cursor
 	$statement->closeCursor();
 	// return result
 	return $data;
+}
+
+function parseSemesterYear($semesterYear) 
+{
+    switch ($semesterYear) {
+        case "Spring 2023":
+            $semester = "Spring";
+            $year = 2023;
+            return [$semester, $year];
+        case "Summer 2023":
+            $semester = "Summer";
+            $year = 2023;
+            return [$semester, $year];
+        case "Fall 2023":
+            $semester = "Fall";
+            $year = 2023;
+            return [$semester, $year];
+        case "Spring 2022":
+            $semester = "Spring";
+            $year = 2022;
+            return [$semester, $year];
+        case "Summer 2022":
+            $semester = "Summer";
+            $year = 2022;
+            return [$semester, $year];
+        case "Fall 2022":
+            $semester = "Fall";
+            $year = 2022;
+            return [$semester, $year];
+        case "Spring 2021":
+            $semester = "Spring";
+            $year = 2021;
+            return [$semester, $year];
+        case "Summer 2021":
+            $semester = "Summer";
+            $year = 2021;
+            return [$semester, $year];
+        case "Fall 2021":
+            $semester = "Fall";
+            $year = 2021;
+            return [$semester, $year];
+        case "Spring 2020":
+            $semester = "Spring";
+            $year = 2020;
+            return [$semester, $year];
+        case "Summer 2020":
+            $semester = "Summer";
+            $year = 2020;
+            return [$semester, $year];
+        case "Fall 2020":
+            $semester = "Fall";
+            $year = 2020;
+            return [$semester, $year];
+        case "Spring 2019":
+            $semester = "Spring";
+            $year = 2019;
+            return [$semester, $year];
+        case "Summer 2019":
+            $semester = "Summer";
+            $year = 2019;
+            return [$semester, $year];
+        case "Fall 2019":
+            $semester = "Fall";
+            $year = 2019;
+            return [$semester, $year];
+    }
 }
 
 function getAllCourses()
--- search.php
+++ search.php
@@ -1,25 +1,20 @@
+<!-- Table design: https://bootsnipp.com/snippets/rz -->
+<!-- Search bar design: https://bootsnipp.com/snippets/35V6b -->
+
 <?php
 require('connect-db.php');
 require('search-db.php');
 
 session_start();
-// if(isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true)
-// {
-//     echo "logged in";
-// } else 
-// {
-//     echo "not logged in!";
-// }
 
 $searchResults = getAllCourses();
-// var_dump($searchResults);
 
 if ($_SERVER['REQUEST_METHOD'] == 'POST')
 {
     if (!empty($_POST['searchBtn']) && ($_POST['searchBtn'] == "Search"))
     {
-        $searchText = $_POST['searchText'];
-        $searchResults = searchCourses($searchText);
+        $postVariables = $_POST;
+        $searchResults = searchCourses($postVariables);
     }
     if (!empty($_POST['PlannerButton']) && isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true)
     {
@@ -56,13 +51,66 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST')
     <div class="container">
         <br/>
         <div class="row justify-content-center">
-            <div class="col-12 col-md-10 col-lg-8">
+            <div class="col-lg-12">
                 <form class="card card-sm" action="search.php" method="post">
                     <div class="card-body row no-gutters align-items-center">
-                        <div class="col">
-                            <input class="form-control form-control-lg form-control-borderless" type="text" name="searchText" placeholder="Search by keyword" required />
+                        <div class="col-lg-3">
+                            <input class="form-control form-control-borderless" type="text" name="searchText" placeholder="Search by keyword" />
                         </div>
                         <!-- Add ability to filter semester/year -->
+                        <div class="col-lg-3 col-md-3 col-sm-12">
+                            <select class="form-control search-slt" name="semesterYearFilter">
+                                <option>Filter by Semester...</option>
+                                <option>Fall 2023</option>
+                                <option>Summer 2023</option>
+                                <option>Spring 2023</option>
+                                <option>Fall 2022</option>
+                                <option>Summer 2022</option>
+                                <option>Spring 2022</option>
+                                <option>Fall 2021</option>
+                                <option>Summer 2021</option>
+                                <option>Spring 2021</option>
+                                <option>Fall 2020</option>
+                                <option>Summer 2020</option>
+                                <option>Spring 2020</option>
+                                <option>Fall 2019</option>
+                                <option>Summer 2019</option>
+                                <option>Spring 2019</option>
+                            </select>
+                        </div>
+                        <div class="col-lg-3 col-md-3 col-sm-12">
+                            <select class="form-control search-slt" name="deptFilter">
+                                <option>Filter by Department...</option>
+                                <option>GOV</option>
+                                <option>STAT</option>
+                                <option>MSE</option>
+                                <option>MATH</option>
+                                <option>APMA</option>
+                                <option>MSE</option>
+                                <option>FREN</option>
+                                <option>ART</option>
+                                <option>ECE</option>
+                                <option>BME</option>
+                                <option>SYSE</option>
+                                <option>MUSI</option>
+                                <option>GERM</option>
+                                <option>CLAS</option>
+                                <option>SUST</option>
+                                <option>PSYC</option>
+                                <option>EDUC</option>
+                                <option>ENGR</option>
+                                <option>ARCH</option>
+                                <option>PLRL</option>
+                                <option>NURS</option>
+                                <option>SPAN</option>
+                                <option>PHYS</option>
+                                <option>CME</option>
+                                <option>CVEE</option>
+                                <option>BIOL</option>
+                                <option>ARCY</option>
+                                <option>JWST</option>
+                            </select>
+                        </div>
                         <div class="col-auto">
                             <input class="btn btn-lg btn-success" type="submit" name="searchBtn" value="Search" />
                         </div>
