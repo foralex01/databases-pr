6b8bbf7f98903d0f27e34b0c6aefac5a6392e6a2
foralex01 <adm8mwh@virginia.edu>
2023-05-01 02:39:34 -0400

    added copies

---
+++ requirements copy.php
@@ -0,0 +1,362 @@
+<?php 
+
+
+require('connect-db.php');
+
+session_start();
+ 
+// Check if the user is already logged in, if yes then redirect him to welcome page
+if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] === false){
+  header("location: login.php"); // change from welcome.php to whatever we want our first page to be
+  exit;
+}
+
+
+require('requirements-function.php');
+
+
+
+
+
+
+
+
+?>
+
+
+
+
+
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>UVA Student Course Planner</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta name="author" content="your name">
+  <meta name="description" content="include some description about your page">      
+  <title>DB interfacing</title>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+  <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+  <link rel="icon" type="image/png" href="http://www.cs.virginia.edu/~up3f/cs4750/images/db-icon.png" />
+</head>
+</head>
+
+<body>
+
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <div class="container-fluid">
+            <div class="navbar-brand">
+                <a class="navbar-brand" href="#">UVA Student Course Planner</a>
+            </div>
+            <div class="collapse navbar-collapse">
+                <ul class="navbar-nav mr-auto">
+                    <li class="nav-item active"><a class="nav-link" href="#">My Planner</a></li>
+                    <li class="nav-item"><a class="nav-link" href="/databases-pr/requirements.php">Degree Requirements</a></li>
+                    <li class="nav-item"><a class="nav-link" href="search.php">Search Courses</a></li>
+                </ul>
+                <ul class="navbar-nav my-2 my-lg-0">
+                    <li class="nav-item"><a class="nav-link" href="#">My Profile</a></li>
+                </ul>
+            </div>
+        </div>
+	</nav>
+
+
+
+<?php include('header.html') ?> 
+
+
+
+
+
+
+
+
+<div class="container">
+
+
+<?php 
+// if (!empty($_POST['name']))
+//    echo greeting($_POST['name']) . "<br/>";
+?>
+
+</br>
+</br>
+
+<div style="text-align:center;">
+<h3>Requirements<h3>
+</div>
+
+</br>
+</br>
+
+ 
+<?
+
+
+
+
+?>
+
+
+
+
+<?php $current_user = findMajorByUser("ht6xd"); 
+
+$major_requirements = displayRequirements($current_user['major_name']); 
+
+$completed_requirements = coursesCompleteRequirements($current_user['major_name'], "ht6xd");
+
+
+
+// echo $x;
+
+?>
+
+
+
+<body>
+
+
+
+<?php 
+
+$x = 0;
+
+foreach ($major_requirements as $req):
+
+  $x = $x + $req['num_required'];
+
+
+
+
+endforeach; 
+
+
+
+$y = 0;
+
+
+foreach ($completed_requirements as $req):
+
+  $y = $y + $req['C'];
+
+
+
+endforeach; 
+
+
+
+
+$result = ($y / $x) * 100;
+
+// echo round($result);
+
+?>
+
+
+
+<div class="w3-light-grey w3-xlarge">
+  <div class="w3-container w3-green" style="width:25%"> 50% </div>
+</div>
+
+</br>
+</br>
+</br>
+
+
+<table class="w3-table w3-bordered w3-card-4 center" style="width:100%">
+
+
+  <thead>
+    <th> Major Requirements - <?php echo $current_user['major_name'];?> </th>
+    <tr style="background-color:#DCEDC8">
+      <th scope="col">Requirement</th>
+      <th scope="col">Number of Requirements </th>
+      
+    </tr>
+  </thead>
+
+
+  <tbody>
+  <?php foreach ($major_requirements as $req): ?>
+    
+      <tr class="table-success">
+        <td class = "tables-success"><?php echo $req['requirement_name']; ?></td>
+        <td><?php echo $req['num_required']; ?></td>
+     
+      </tr>
+  
+  <?php endforeach; ?>
+   
+    </tr>
+  </tbody>
+</table>
+
+  </br>
+  </br>
+  </br>
+  </br>
+  </br>
+
+<?php $completed_requirements = coursesCompleteRequirements($current_user['major_name'], "ht6xd"); ?>
+
+
+<table class="w3-table w3-bordered w3-card-4 center" style="width:100%">
+
+
+  <thead>
+    <th> Remaining Requirements: </th>
+    <tr style="background-color:#DCEDC8">
+      <th scope="col">Requirement</th>
+      <th scope="col">Number of Requirements </th>
+      <th scope="col">Currently Being Fulfilled By: </th>
+      <th scope="col">Possible Courses to Fulfill these Requirements: </th>
+    </tr>
+  </thead>
+
+
+  <tbody>
+
+  
+
+  <!-- displaying remaining requirements -->
+  <?php foreach ($major_requirements as $req):
+
+
+    
+    
+    $x = 0;
+
+    $h = classes($current_user['major_name'], "ht6xd", $req['requirement_name']); 
+
+    $a = countDifference($current_user['major_name'], "ht6xd", $req['requirement_name']); 
+
+
+    $m = "";
+    $n = "";
+
+    $d = "";
+    $z = "";
+
+    foreach ($h as $count):
+
+      
+      $m = $m .= $count['dept_abbr'];
+      $n = $n .= $count['course_code'];
+
+    
+
+    endforeach;
+
+    
+
+    
+    foreach ($a as $count):
+
+      $x = $x + $count['C'];
+      $d = $d .= $count['dept_abbr'];
+      $z = $z .= $count['course_code'];
+
+    endforeach;
+
+    ?>
+    
+      <tr class="table-success">
+        <td class = "tables-success"><?php echo $req['requirement_name']; ?></td>
+        <td><?php echo $req['num_required'] - $x; ?></td>
+        <td><?php echo $d .=$z; ?></td>
+        <td><?php echo $m .=$n; ?></td>
+     
+      </tr>
+  
+  <?php endforeach; ?>
+   
+    </tr>
+  </tbody>
+</table>
+
+
+
+<!-- fulfilled by dept and course codes where course_fulfills_requirement.requirement = requirement ,, display codes-->
+
+<!-- completed requirements allows me to see what has been yet been completed -->
+
+<!-- use the C to filter the number that have been completed -->
+
+
+</br>
+</br>
+
+
+
+
+
+
+
+<!-- function that shows the difference between the already completed requirements 
+and the requirements that need to be met -->
+
+<!-- return count where requiremernt_name == ___ -->
+
+
+
+<br/>
+
+
+
+
+
+
+
+
+<?php
+
+
+
+//first display all requirements
+//return that requriement name 
+// then use that requirement name to find the number
+
+
+
+
+$major_requirements = displayRequirements($current_user['major_name']); 
+
+//find difference in requirements
+
+
+
+// ?>
+
+
+
+
+
+
+
+<?php include('footer.html') ?>
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+
+
+
+
+
+</body>
+</html>
+
+
+  
+
+
+
+<?php
+// function greeting($name)
+// {
+//    return "Hi, " . $_POST['name'];
+// }
+
+
+?>
---
+++ requirements-function copy.php
@@ -0,0 +1,272 @@
+<?php
+
+// CREATE TABLE friends (
+//    name varchar(30) NOT NULL,
+//    major varchar(10) NOT NULL,
+//    year int NOT NULL,
+//    PRIMARY KEY (name) );
+
+// Prepared statement (or parameterized statement) happens in 2 phases:
+//   1. prepare() sends a template to the server, the server analyzes the syntax
+//                and initialize the internal structure.
+//   2. bind value (if applicable) and execute
+//      bindValue() fills in the template (~fill in the blanks.
+//                For example, bindValue(':name', $name);
+//                the server will locate the missing part signified by a colon
+//                (in this example, :name) in the template
+//                and replaces it with the actual value from $name.
+//                Thus, be sure to match the name; a mismatch is ignored.
+//      execute() actually executes the SQL statement
+
+
+function getAllDepartments()
+{
+	global $db;
+	$query = "SELECT * FROM Department";
+	$statement = $db->prepare($query);
+	$statement->execute();
+	
+	// fetchAll() returns an array for all of the rows in the result set
+	$results = $statement->fetchAll();
+	
+	// closes the cursor and frees the connection to the server so other SQL statements may be issued
+	$statement->closecursor();
+	
+	return $results;
+}
+
+function displayRequirements($major)
+{
+
+	global $db;
+	$query = "SELECT * FROM Requirement WHERE major_name = '".$major."'";
+	$statement = $db->prepare($query);
+	$statement->execute();
+	
+	
+	$results = $statement->fetchAll();
+	
+	
+	$statement->closecursor();
+	
+	return $results;
+
+}
+
+
+
+// function that from the courses already taken, displays what is left
+function coursesCompleteRequirements($major, $cid)
+{
+
+	global $db;
+
+	// courses = find where dept, abbr FROM Student_Takes_Course (WHERE cid = what is passed in and 
+	// major = what is passed in)
+	// INTERSECTS with dept, abbr 
+	// intersects with dept, abbr FROM Course_Fulfills Requirement WHERE department = major
+
+	// ^ find the courses that complete the major requirements
+	
+	// next find the count of count of each requriement that is fulfilled 
+
+	// how many of each requirement for the major is fulfilled?
+
+	//SELECT count(courses) FROM Course_fulfills_requirements
+	// GROUP BY requirment name
+
+	//^ this gives me the number of each requirement I have completed
+
+	//^ once I find this, I can compare this number to the 
+	// number of each requirement that the make requires
+
+	// and then show the remaining credits they need of each one 
+
+
+	//FROM Student_Takes_Couse
+	//WHERE cid = "adm8mwh" AND major = "Biomedical Engineering" AND Student_Takes_Course.dept_abbr 
+	//IN (SELECT Course_Fulfills_Requirement.debt_abbr FROM COURSE_Fulfills_Requirement
+	//WHERE Course_Fulfills_Requirement.debt_abbr = "BME")";
+
+	
+	$query = "SELECT cid, major_name, Course_Fulfills_Requirement.dept_abbr, Course_Fulfills_Requirement.course_code, requirement_name, COUNT(Course_Fulfills_Requirement.course_code) AS C FROM Course_Fulfills_Requirement, Student_Takes_Course WHERE
+	cid = :cid AND Course_Fulfills_Requirement.course_code = Student_Takes_Course.course_code AND
+	Course_Fulfills_Requirement.dept_abbr = Student_Takes_Course.dept_abbr
+	GROUP BY requirement_name HAVING major_name = :major";
+
+	//display the number of each requirement that is required for the major 
+
+	//compare the requirements that have been fulfilled to the requirements that haven't been fulfilled
+
+	//GROUP BY requirement
+
+	// COUNT(course_codes)
+
+	// ^ this will find the number of requirements filled for each requirement
+	// ^ 
+	
+	
+
+	
+	
+
+	$statement = $db->prepare($query);
+	$statement->bindValue(':major', $major);
+	$statement->bindValue(':cid', $cid);
+	$statement->execute();
+
+	//fetchAll() returns an array for all of the rows in the result set
+	$results = $statement->fetchAll();
+
+	// closes the cursor and frees the connection to the server so other SQL statements may be issued
+	$statement->closecursor();
+
+	return $results;
+
+
+}
+
+// function where you subtract (by requirement) the requirements taken from the requirement needed
+
+
+
+function findMajorByUser($computingid)
+{
+
+	global $db;
+	
+	$query = "SELECT major_name FROM Student_In_Major WHERE cid = :computingid";
+	$statement = $db->prepare($query);
+	$statement->bindValue(':computingid', $computingid);
+	$statement->execute();
+	
+	// fetchAll() returns an array for all of the rows in the result set
+	// fetch() return a row
+	$results = $statement->fetch();
+	
+	// closes the cursor and frees the connection to the server so other SQL statements may be issued
+	$statement->closecursor();
+	
+	return $results;
+
+	
+
+}
+
+
+function courses()
+{
+
+    global $db;
+
+    //computing id is as passed in and courses_have_been_taken by the computing ID
+	
+	$query = "SELECT * FROM Course_Fulfills_Requirement";
+    
+    //.course_code, Course_Fulfills_Requirement.dept_abbr FROM
+    //Course_Fulfills_Requirement, Student_Takes_Course WHERE cid = :cid";
+    
+    //AND requirement_name = :requirement
+    //AND major = :major AND Student_Takes_Course.dept_abbr = Course_Fulfills_Requirement.dept_abbr AND
+    //Student_Takes_Course.course_code = Course_Fulfills_Requirement.dept_abbr";
+
+	$statement = $db->prepare($query);
+    
+	//$statement->bindValue(':requirement', $requirement);
+    //$statement->bindValue(':major', $major);
+    // $statement->bindValue(':cid', $cid);
+	$statement->execute();
+	
+	// fetchAll() returns an array for all of the rows in the result set
+	// fetch() return a row
+	$results = $statement->fetchAll();
+	
+	// closes the cursor and frees the connection to the server so other SQL statements may be issued
+	$statement->closecursor();
+	
+	return $results;
+
+
+
+
+}
+
+function countDifference($major, $cid, $r_name)
+{
+    global $db;
+
+
+    $query = "SELECT cid, major_name, Course_Fulfills_Requirement.dept_abbr, Course_Fulfills_Requirement.course_code, requirement_name, COUNT(Course_Fulfills_Requirement.course_code) AS C FROM Course_Fulfills_Requirement, Student_Takes_Course WHERE
+	cid = :cid AND Course_Fulfills_Requirement.course_code = Student_Takes_Course.course_code AND
+	Course_Fulfills_Requirement.dept_abbr = Student_Takes_Course.dept_abbr
+	GROUP BY requirement_name HAVING major_name = :major AND requirement_name = :r_name";
+
+
+    $statement = $db->prepare($query);
+
+    $statement->bindValue(':major', $major);
+
+	$statement->bindValue(':cid', $cid);
+
+    $statement->bindValue(':r_name', $r_name);
+
+    $statement->execute();
+
+    $results = $statement->fetchAll();
+
+    $statement->closecursor();
+
+    return $results;
+
+}
+
+function classes($major, $cid, $r_name)
+{
+
+    global $db;
+
+
+    $query = "SELECT cid, major_name, Course_Fulfills_Requirement.dept_abbr, Course_Fulfills_Requirement.course_code, requirement_name
+    FROM Course_Fulfills_Requirement, Student_Takes_Course WHERE
+	cid = :cid AND Course_Fulfills_Requirement.course_code <> Student_Takes_Course.course_code AND
+	Course_Fulfills_Requirement.dept_abbr <> Student_Takes_Course.dept_abbr
+    GROUP BY requirement_name HAVING major_name = :major AND requirement_name = :r_name";
+	
+    //GROUP BY requirement_name HAVING major_name = :major AND requirement_name = :r_name";
+
+
+    $statement = $db->prepare($query);
+
+    $statement->bindValue(':major', $major);
+
+	$statement->bindValue(':cid', $cid);
+
+    $statement->bindValue(':r_name', $r_name);
+
+    $statement->execute();
+
+    $results = $statement->fetchAll();
+
+    $statement->closecursor();
+
+    return $results;
+
+
+
+}
+
+
+
+
+
+// function that shows the difference between the already completed requirements 
+// and the requirements that need to be met
+
+// requirements met by requirement_name
+
+// requirements not met by requirement_name
+
+
+
+
+?>
