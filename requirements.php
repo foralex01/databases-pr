06ee0955315114d720e142f5d8b2210f6108ba5f
Henry Todd <htodd2001@gmail.com>
2023-05-01 07:26:11 -0400

    making boostrap consistent for each page

--- requirements.php
+++ requirements.php
@@ -16,20 +16,7 @@ else {
 }
 
 require('requirements-function.php');
-
-
-
-
-
-
-
-
 ?>
-
-
-
-
-
 
 <!DOCTYPE html>
 <html>
@@ -42,42 +29,17 @@ require('requirements-function.php');
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
   <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
   <link rel="icon" type="image/png" href="http://www.cs.virginia.edu/~up3f/cs4750/images/db-icon.png" />
-</head>
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <style>
+        body{ font: 14px sans-serif; }
+        .wrapper{ width: 360px; padding: 20px; }
+    </style>
 </head>
 
 <body>
-
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <div class="container-fluid">
-            <div class="navbar-brand">
-                <a class="navbar-brand" href="#">UVA Student Course Planner</a>
-            </div>
-            <div class="collapse navbar-collapse">
-                <ul class="navbar-nav mr-auto">
-                    <li class="nav-item active"><a class="nav-link" href="planner.php">My Planner</a></li>
-                    <li class="nav-item"><a class="nav-link" href="/databases-pr/requirements.php">Degree Requirements</a></li>
-                    <li class="nav-item"><a class="nav-link" href="search.php">Search Courses</a></li>
-                </ul>
-                <ul class="navbar-nav my-2 my-lg-0">
-                    <li class="nav-item"><a class="nav-link" href="#">My Profile</a></li>
-                </ul>
-            </div>
-        </div>
-	</nav>
-
-
-
-<?php include('header.html') ?> 
-
-
-
-
-
-
-
-
+<?php include("navbar.php"); ?>
+<!-- <?php include('header.html') ?>  -->
 <div class="container">
-
 
 <?php 
 // if (!empty($_POST['name']))
@@ -88,77 +50,37 @@ require('requirements-function.php');
 </br>
 
 <div style="text-align:center;">
-<h3>Degree Requirements Progress <h3>
+<h2>Degree Requirements Progress <h2>
 </div>
 
-</br>
-</br>
-
- 
-<?
-
-
-
-
-?>
-
-
-
-
-<?php $current_user = findMajorByUser($_SESSION["cid"]); 
-
-
+<?php 
+$current_user = findMajorByUser($_SESSION["cid"]); 
 $major_requirements = displayRequirements($current_user['major_name']); 
-
 $completed_requirements = coursesCompleteRequirements($current_user['major_name'],$_SESSION["cid"]);
 
-
-
-// echo $x;
-
-?>
-
-
-
-
-
-
-<?php 
-
+//check num to take
 $x = 0;
 
 foreach ($major_requirements as $req):
 
   $x = $x + $req['num_required'];
 
-
-
-
 endforeach; 
 
 
-
+//check num taken
 $y = 0;
-
 
 foreach ($completed_requirements as $req):
 
   $y = $y + $req['C'];
 
-
-
 endforeach; 
 
-
-
-
-$result = ($y / $x) * 100;
-
-// echo round($result);
+//progress
+$result = ($y / $x) * 100
 
 ?>
-
-
 
 <!-- <div class="w3-light-grey w3-xlarge">
   <div class="w3-container w3-green" style="width:25%"> 50% </div>
@@ -168,24 +90,19 @@ $result = ($y / $x) * 100;
 </br>
 </br>
 
-
 <table class="w3-table w3-bordered w3-card-4 center" style="width:100%">
-
-
   <thead>
     <th> Major Requirements - <?php echo $current_user['major_name'];?> </th>
     <tr style="background-color:#DCEDC8">
       <th scope="col">Requirement</th>
       <th scope="col">Number of Requirements </th>
-      
     </tr>
   </thead>
-
 
   <tbody>
   <?php foreach ($major_requirements as $req): ?>
     
-      <tr class="table-success">
+      <tr class="table-neutral">
         <td class = "tables-success"><?php echo $req['requirement_name']; ?> </br></td>
         <td><?php echo $req['num_required']; ?></br></td>
      
@@ -205,9 +122,7 @@ $result = ($y / $x) * 100;
 
 <?php $completed_requirements = coursesCompleteRequirements($current_user['major_name'], $_SESSION["cid"]); ?>
 
-
 <table class="w3-table w3-bordered w3-card-4 center" style="width:100%">
-
 
   <thead>
     <th> Remaining Requirements: </th>
@@ -218,24 +133,16 @@ $result = ($y / $x) * 100;
       <th scope="col">Possible Courses to Fulfill these Requirements: </th>
     </tr>
   </thead>
-
-
   <tbody>
-
-  
 
   <!-- displaying remaining requirements -->
   <?php foreach ($major_requirements as $req):
-
-
-    
     
     $x = 0;
 
     $h = classes($current_user['major_name'], $_SESSION["cid"], $req['requirement_name']); 
 
     $a = countDifference($current_user['major_name'], $_SESSION["cid"], $req['requirement_name']); 
-
 
     $m = "";
     $n = "";
@@ -244,16 +151,11 @@ $result = ($y / $x) * 100;
     $z = "";
 
     foreach ($h as $count):
-
-      
+  
       $m = $m .= $count['dept_abbr'];
       $n = $n .= $count['course_code'];
 
-    
-
     endforeach;
-
-    
 
     
     foreach ($a as $count):
@@ -266,7 +168,7 @@ $result = ($y / $x) * 100;
 
     ?>
     
-      <tr class="table-success">
+      <tr class="table-neutral">
         <td class = "tables-success"><?php echo $req['requirement_name']; ?></td>
         <td><?php echo $req['num_required'] - $x; ?></td>
         <td><?php echo $d;?> <?php echo " ";?> <?php echo $z; ?></td>
@@ -309,7 +211,7 @@ and the requirements that need to be met -->
 <br/>
 
 
-<table class="w3-table w3-bordered w3-card-4 center" style="width:100%">
+<!-- <table class="w3-table w3-bordered w3-card-4 center" style="width:100%">
 
 
   <thead>
@@ -339,7 +241,7 @@ and the requirements that need to be met -->
    
     </tr>
   </tbody>
-</table>
+</table> -->
 
 
 
@@ -370,7 +272,7 @@ and the requirements that need to be met -->
 
 
 
-<?php include('footer.html') ?>
+<!-- <?php include('footer.html') ?> -->
 
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
 
--- search.php
+++ search.php
@@ -91,6 +91,11 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST')
     <meta charset="UTF-8">
     <meta name="author" content="Alexandra Martin, Henry Todd, Matthew Lunsford, Rebecca Chung"> 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <style>
+        body{ font: 14px sans-serif; }
+        .wrapper{ width: 360px; padding: 20px; }
+    </style>
 </head>
 <body>
     <?php include('navbar.php');?>
