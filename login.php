692ea0399911f8e4a2ef53a82e513e77aac54f48
Henry Todd <htodd2001@gmail.com>
2023-05-01 00:07:19 -0400

    quick rerouting fix

--- login.php
+++ login.php
@@ -62,7 +62,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST") {
                             $_SESSION["cid"] = $cid;                         
                             
                             // Redirect user to welcome page
-                            header("location: home.php");
+                            header("location: planner.php");
                         } else {
                             // Password is not valid, display a generic error message
                             $login_err = "Invalid username or password.";
